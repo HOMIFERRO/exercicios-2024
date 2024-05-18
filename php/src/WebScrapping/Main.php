dd88c4a7b03eb86074a40c9e93bf0cfbba93807d
Igor <joaoigorgv@gmail.com>
2024-05-18 15:28:25 -0300

    scrapTags made

--- php/src/WebScrapping/Main.php
+++ php/src/WebScrapping/Main.php
@@ -7,12 +7,14 @@ use DOMXPath;
 /**
  * Runner for the Webscrapping exercice.
  */
-class Main {
+class Main
+{
 
   /**
    * Main runner, instantiates a Scrapper and runs.
    */
-  public static function run(): void {
+  public static function run(): void
+  {
     $dom = new \DOMDocument('1.0', 'utf-8');
     $dom->loadHTMLFile(__DIR__ . '/../../assets/origin.html');
 
@@ -21,34 +23,39 @@ class Main {
     // Write your logic to save the output file bellow.
     print_r($data);
     self::scrapVolumeInfo($dom);
+    self::scrapTitles($dom);
   }
-  private static function scrapVolumeInfo(\DOMDocument $dom): array {
+  private static function scrapVolumeInfo(\DOMDocument $dom): array
+  {
 
     $xpath = new DOMXPath($dom);
     $elements = $xpath->query('//div[@class="volume-info"]');
-    
+
     $volumeInfo = [];
     foreach ($elements as $element) {
-      echo $element->nodeValue. PHP_EOL;
+      echo $element->nodeValue . PHP_EOL;
       $volumeInfo[] = [
-        'volumeInfo'=> $element->nodeValue
+        'volumeInfo' => $element->nodeValue
       ];
     }
     return $volumeInfo;
   }
-  private static function scrapTitles(\DOMDocument $dom): array {
-    
+  private static function scrapTitles(\DOMDocument $dom): array
+  {
+
     $xpath = new DOMXPath($dom);
     $elements = $xpath->query('//h4[@class="my-xs paper-title"]');
 
     $titles[] = [];
     foreach ($elements as $element) {
-      echo $element->nodeValue. PHP_EOL;
+      echo $element->nodeValue . PHP_EOL;
       $titles[] = [
         'titles' => $element->nodeValue
       ];
     }
     return $titles;
   }
-
+  private static function scrapTags(\DOMDocument $dom) {
+    
+  }
 }
